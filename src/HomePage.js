c6d5a57a20988e266e4e5778473e4c52f15b6d41
don <40864187+binaruappuhamy@users.noreply.github.com>
2022-02-05 20:27:09 -0500

    Complete about me page

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,8 +5,18 @@ class HomePage extends React.Component {
   render() {
     return (
       <div>
-        {/* Complete your training in here*/}
-        <div>Welcome to VEEP Website!</div>
+        <div align='center'>
+          <img src="uoft_logo.jpg" alt="UofT Logo" width="179" height="79" />
+          <h1>Welcome to VEEP Website!</h1>
+        </div>
+        <div>
+          <h2><u>About me</u></h2>
+              <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;<b>Name :</b> Binaru Appuhamy<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;<b>Current year :</b> ECE PEY<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;<b>Hobbies :</b> Read manga and watch anime!<br />
+              </p>
+        </div>
       </div>
     )
   }

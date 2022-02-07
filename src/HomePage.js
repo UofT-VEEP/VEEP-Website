8cdc0d395afe88d137f59b00784d6b146ea9a2a7
davidYichengWei <a1093888167@hotmail.com>
2022-02-06 19:40:37 -0500

    Completed the 1st training session

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,10 +3,17 @@ import React from 'react';
 class HomePage extends React.Component {
   
   render() {
+    document.title = "Yicheng Wei";
     return (
       <div>
         {/* Complete your training in here*/}
-        <div>Welcome to VEEP Website!</div>
+        <img src="logo192.png"/>
+        <h1>Yicheng Wei</h1>
+        <p>
+          ECE 2T2 + PEY<br/>
+          <br/>
+          Hi I'm Yicheng, I Love playing ukulele and guitar
+        </p>
       </div>
     )
   }

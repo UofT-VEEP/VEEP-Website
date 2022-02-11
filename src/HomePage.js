a160e956fb79d7786b1eba458ae018de78a80ed8
justinzhong7789 <justinzhong7789@gmail.com>
2022-02-10 23:02:31 -0500

    First exercise - personal homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,7 +6,12 @@ class HomePage extends React.Component {
     return (
       <div>
         {/* Complete your training in here*/}
+        
         <div>Welcome to VEEP Website!</div>
+        <h1> Justin Zhong's about page </h1>
+        <p>Hello, my name is Justin. I'm a 3rd year ECE student currently doing my PEY.</p>
+        <img src="logo512.png" alt=""/>
+        
       </div>
     )
   }

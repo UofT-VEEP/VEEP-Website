6b7532eb6b97f2b4995aaecaaa77735180af7177
Yuhao Guan <2200733521@qq.com>
2022-02-13 13:10:41 -0500

    training01

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,6 +7,21 @@ class HomePage extends React.Component {
       <div>
         {/* Complete your training in here*/}
         <div>Welcome to VEEP Website!</div>
+
+        <br></br>
+        <hr></hr>
+        <h3>Here's training a. Add an image of your choice</h3>
+        <img src="/src/logo.svg" alt="logo for something" sytle={{width:"128px", height:"128px"}}/>
+
+        <br></br>
+        <hr></hr>
+        <h3>Here's traning b. Add a tilte of your name</h3>
+        <h1 sytle="font-size:200px;">Yuhao Guan</h1>
+
+        <br></br>
+        <hr></hr>
+        <h3>Here's traning c. Add a shor inroduction aobut yourself</h3>
+        <p>Hey guys! I'm Kevin, and I'm a second year ECE student.</p>
       </div>
     )
   }

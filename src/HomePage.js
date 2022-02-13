e5be41d772d0805e6ebb4b17f4cb1095178a2325
mar-gaux <98907507+mar-gaux@users.noreply.github.com>
2022-02-13 18:54:04 -0500

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,7 +6,11 @@ class HomePage extends React.Component {
     return (
       <div>
         {/* Complete your training in here*/}
-        <div>Welcome to VEEP Website!</div>
+        <div>Welcome to VEEP Website!
+        <h1>Margaux Roncière</h1>
+        <p>Hi! My name is Margaux and I'm in Indy. Here's a picture of a dog:</p>
+        <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*" alt="Dog!!!"></img>
+        </div>
       </div>
     )
   }

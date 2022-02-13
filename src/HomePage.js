bc66a3107b372c55c8043ec1c8b17a88c8cd917c
Cuiyushan Ni <nicuiyushan@gmail.com>
2022-02-13 17:42:21 -0500

    task1

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,6 +7,9 @@ class HomePage extends React.Component {
       <div>
         {/* Complete your training in here*/}
         <div>Welcome to VEEP Website!</div>
+        <img src="123.jpg" alt="Kaka"></img>
+        <h1>Cuiyushan Ni</h1>
+        <p>ECE2T3, Kaka is my favourate soccer player.</p>
       </div>
     )
   }

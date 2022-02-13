f435779f80fef40c183dc8e295411e91b44cb5fe
Ines Qian <inesqyx@gmail.com>
2022-02-13 18:38:59 -0500

    Task1

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,7 +5,9 @@ class HomePage extends React.Component {
   render() {
     return (
       <div>
-        {/* Complete your training in here*/}
+        <img src="M1_IQ_img.jpg" alt="M1_IQ_img"></img>
+        <h3>Name: Ines Qian</h3>
+        <p1><b>I am currently a second-year ECE student at UofT.</b></p1>
         <div>Welcome to VEEP Website!</div>
       </div>
     )

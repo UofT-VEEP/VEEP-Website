1d30409445a05ffb80add00423bb33110db2b920
yixian.zuo <zuoyixian@gmail.com>
2022-02-13 17:04:37 -0500

    Modified branch testing

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,7 +6,13 @@ class HomePage extends React.Component {
     return (
       <div>
         {/* Complete your training in here*/}
-        <div>Welcome to VEEP Website!</div>
+        <img src="https://i.ytimg.com/vi/Nlbc0RbVjM8/maxresdefault.jpg" alt="Snoopy" width="400" height="250"/>
+        <h3>Hello, this is Iris</h3>
+        <p>I love cats</p>
+        {/* Add an image of your choice 
+            Add a title of your name 
+            Add a short introduction about yourself 
+        */}
       </div>
     )
   }

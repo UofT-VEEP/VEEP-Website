d15f2f4b8515cbf6aa62bd164000e912325a50f3
CatherineZM <49298813+CatherineZM@users.noreply.github.com>
2022-02-13 19:00:38 -0500

    Fixed security problem

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class HomePage extends React.Component {
   
   render() {
     return (
       <div>
-        {/* Complete your training in here*/}
+        {/* Complete your Week 2 training in here*/}
         <div>Welcome to VEEP Website!</div>
       </div>
     )

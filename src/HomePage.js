3955a39034dfa25e558ab08747d0f4cdf2c37bc2
CatherineZM <49298813+CatherineZM@users.noreply.github.com>
2022-02-18 02:11:50 -0500

    Week 3 and Week 4 training code

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,12 +3,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class HomePage extends React.Component {
+  // Un-comment this for week 4 content 
+  // constructor(props){
+  //   super(props);
+
+  //   this.state = {}
+  // }
   
   render() {
     return (
       <div>
-        {/* Complete your Week 2 training in here*/}
-        <div>Welcome to VEEP Website!</div>
+        
       </div>
     )
   }

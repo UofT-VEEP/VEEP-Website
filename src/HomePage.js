bfdd9b5f67a96fb6b603ded6a38a89eed8a962ac
davidYichengWei <a1093888167@hotmail.com>
2022-02-19 18:40:35 -0500

    Completed 2nd training

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/navbar';
+import Nav from 'react-bootstrap/Nav';
+import Accordion from 'react-bootstrap/Accordion';
 
 class HomePage extends React.Component {
   // Un-comment this for week 4 content 
@@ -11,10 +16,41 @@ class HomePage extends React.Component {
   // }
   
   render() {
+    document.title = "VEEP";
     return (
-      <div>
+      <Container fluid>
+        <Navbar bg="dark" expand="lg" variant = "dark">
+          <Container>
+            <Navbar.Brand href="#home">U of T VEEP</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#Projects">Projects</Nav.Link>
+              <Nav.Link href="#About Us">About Us</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Card border = "dark" style={{ width: '20rem'}}>
+          <Card.Body>
+            <Card.Img variant = "top" src = "logo192.png" alt = "logo"/>
+            <Card.Title>Yicheng Wei</Card.Title>
+            <Card.Text>
+              ECE 2T2 + PEY <br/>
+              Hi I'm Yicheng, I Love playing ukulele and guitar
+            </Card.Text>
+            <Accordion defaultActiveKey="0">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header> View More </Accordion.Header>
+                <Accordion.Body>
+                  I also have a cute cat
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Card.Body>  
+        </Card>
+      </Container>
         
-      </div>
+      
     )
   }
 }

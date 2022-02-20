e2117679c36b12069fa502512a3e5c3f1313094c
don <40864187+binaruappuhamy@users.noreply.github.com>
2022-02-20 17:06:25 -0500

    Complete assignment 2

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 class HomePage extends React.Component {
   // Un-comment this for week 4 content 
   // constructor(props){
@@ -13,17 +18,33 @@ class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <div align='center'>
-          <img src="uoft_logo.jpg" alt="UofT Logo" width="179" height="79" />
-          <h1>Welcome to VEEP Website!</h1>
-        </div>
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand >UofT Veep</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link >Home</Nav.Link>
+              <Nav.Link >Projects</Nav.Link>
+              <Nav.Link >About Us</Nav.Link>
+            </Nav>
+          </Container>   
+        </Navbar>
         <div>
-          <h2><u>About me</u></h2>
-              <p>
-                &nbsp;&nbsp;&nbsp;&nbsp;<b>Name :</b> Binaru Appuhamy<br />
-                &nbsp;&nbsp;&nbsp;&nbsp;<b>Current year :</b> ECE PEY<br />
-                &nbsp;&nbsp;&nbsp;&nbsp;<b>Hobbies :</b> Read manga and watch anime!<br />
-              </p>
+            <Card style={{ width: '15rem' }}>
+              <Card.Img variant="top" src="uoft_logo.jpg" alt="UofT Logo" />
+              <Card.Body>
+                <Card.Title><b><u>About me</u></b></Card.Title>
+                <Card.Text>
+                  <Container>
+                    <b>Name :</b> Binaru Appuhamy<br />
+                    <b>Current year :</b> ECE PEY<br />
+                    <b>Hobbies :</b> Read manga and watch anime!<br />
+                  </Container>
+                </Card.Text>
+                  <DropdownButton id="dropdown-basic-button" title="More Hobbies">
+                    <Dropdown.Item href="more_hobbies.gif" >Click here to get admin permission to view this information.</Dropdown.Item>
+                  </DropdownButton>
+              </Card.Body>
+            </Card>
         </div>
       </div>
     )

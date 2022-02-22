0b0b4b4ecf4a2087044dfc2c3ec860e328348e14
don <40864187+binaruappuhamy@users.noreply.github.com>
2022-02-22 00:25:24 -0500

    Complete Assignment 3

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,6 +10,16 @@ class NavBar extends React.Component {
   render() {
     return (
       <div>
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand >UofT Veep</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link >Home</Nav.Link>
+              <Nav.Link >Projects</Nav.Link>
+              <Nav.Link >About Us</Nav.Link>
+            </Nav>
+          </Container>   
+        </Navbar>
       </div>
     )
   }

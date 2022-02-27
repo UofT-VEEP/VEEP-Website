e585349c2827e4d3c788437899c049ac2520c46b
justinzhong7789 <justinzhong7789@gmail.com>
2022-02-27 03:02:17 -0500

    Assignment 2. Added card and navbar component.

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Accordion, Navbar, Nav, Container } from 'react-bootstrap';
 
 class HomePage extends React.Component {
   // Un-comment this for week 4 content 
@@ -13,7 +14,36 @@ class HomePage extends React.Component {
   render() {
     return (
       <div>
-        
+        <Navbar bg="primary" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">UofT VEEP</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#other">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="logo512.png" />
+          <Card.Body>
+            <Card.Title>Justin Zhong's about page</Card.Title>
+            <Card.Text>
+              Hello, my name is Justin. I'm a 3rd year ECE student
+            </Card.Text>
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>View More</Accordion.Header>
+                <Accordion.Body>
+                  Not gonna let you know more about me :D
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Card.Body>
+        </Card>
       </div>
     )
   }

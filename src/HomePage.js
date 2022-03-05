384f10a8c60aa1b558cde9940fb1a73fa8d94e59
Yuhao <2200733521@qq.com>
2022-03-05 09:17:16 -0500

    Training 03

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,8 +2,6 @@ import React from 'react';
 import NavBar from './Components/NavBar'; 
 import { Card, Accordion } from 'react-bootstrap';
 import './Styles/HomePage.css';
-import { Card, Accordion, Navbar, Nav, Container } from 'react-bootstrap';
-
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -65,39 +63,6 @@ class HomePage extends React.Component {
             </Card.Body>
           </Card> 
         </div>
-        <Navbar bg="success" variant="dark">
-          <Container>
-          <Navbar.Brand href="#home">UofT VEEP</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#aboutUs">About Us</Nav.Link>
-          </Nav>
-          </Container>
-        </Navbar>
-
-        <br/>
-
-        <Card border="success" style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="KG01.png" alt="Kevin" width="286" height="161" />
-          <Card.Body>
-            <Card.Title>Yuhao Guan</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">2nd Year ECE</Card.Subtitle>
-            <Card.Text>
-              My name is Kevin. I like mining.
-            </Card.Text>
-
-            <Accordion>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>View More</Accordion.Header>
-                <Accordion.Body>
-                  I am a friend of Steve.
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-
-          </Card.Body>
-        </Card>
       </div>
     )
   }

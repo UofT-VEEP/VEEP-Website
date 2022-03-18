bc2ba2b77fd9bacbcca4d34d8775da329e69f7a8
Yuhao Guan <2200733521@qq.com>
2022-03-18 06:21:30 -0400

    Train04

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import NavBar from './Components/NavBar'; 
-import { Card, Accordion } from 'react-bootstrap';
+import { Card, Accordion, Button, Toast } from 'react-bootstrap';
 import './Styles/HomePage.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class HomePage extends React.Component {
-  // Un-comment this for week 4 content 
-  // constructor(props){
-  //   super(props);
+  constructor(props){
+    super(props);
+    this.state = { 
+      showHC2Toast: false,
+    };
+    this.HC2Button = this.HC2Button.bind(this);
+  }
 
-  //   this.state = {}
-  // }
+  HC2Button(){
+    const click = !this.state.showHC2Toast;
+    this.setState({
+      showHC2Toast: click,
+    });
+  }
   
   render() {
     return (
@@ -20,7 +28,7 @@ class HomePage extends React.Component {
                 
         {/************************* Card #01 **********************************/}
         <div className="leftcolumn">
-          <Card className="Card">
+          <Card className="HomeCard">
             <Card.Body>
               <Card.Img variant="top" src="KG01.png" alt="Kevin" className="Card_Img"/>
               <Card.Title className="Card_Title"><br/>Yuhao Guan</Card.Title>
@@ -37,13 +45,16 @@ class HomePage extends React.Component {
                   </Accordion.Body>
                 </Accordion.Item>
               </Accordion>
+
             </Card.Body>
           </Card>
         </div>
 
+
+
         {/************************* Card #02 **********************************/}
         <div className="rightcolumn">
-          <Card className="Card">
+          <Card className="HomeCard">
             <Card.Body>
               <Card.Img variant="top" src="KG02.png" alt="Kevin Logo" className="Card_Img2"/>
               <Card.Title className="Card_Title"><br></br>KG LOGO</Card.Title>
@@ -52,14 +63,19 @@ class HomePage extends React.Component {
                 I'm a logo.
               </Card.Text>
 
-              <Accordion>
-                <Accordion.Item eventKey="0">
-                  <Accordion.Header>View More</Accordion.Header>
-                  <Accordion.Body>
-                    I'm a logo.
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Accordion>
+              <Button onClick={this.HC2Button} variant="success" size="lg">
+                Whose logo?
+              </Button>
+
+              <Toast className="Toasts" show={this.state.showHC2Toast} onClose={this.HC2Button}>
+                <Toast.Header>
+                  <img src="KG03.jpg" className="Toasts_Img" alt="Kevin Logo"/>
+                  <strong className="me-auto"> | KG </strong>
+                  <small> just now </small>
+                </Toast.Header>
+                <Toast.Body> Mine :D </Toast.Body>
+              </Toast>              
+
             </Card.Body>
           </Card> 
         </div>

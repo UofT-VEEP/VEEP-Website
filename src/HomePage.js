140a983487919176897c2b4f92515fe882f166d8
don <40864187+binaruappuhamy@users.noreply.github.com>
2022-03-20 17:41:40 -0400

    Complete training 4

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,42 +1,41 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Accordion from 'react-bootstrap/Accordion';
-import Container from 'react-bootstrap/Container';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Components/NavBar';
 import './Styles/HomePage.css';
+import NavBar from './Components/NavBar';
+import {Card, Accordion, Container, Button, Toast} from 'react-bootstrap';
 
 class HomePage extends React.Component {
-  // Un-comment this for week 4 content 
-  // constructor(props){
-  //   super(props);
-
-  //   this.state = {}
-  // }
+  // Un-comment this for week 4 content
+  constructor(props){
+    super(props);
+    this.state = {
+      show : true
+    }
+    this.toggleshow = () => this.setState({show : !this.state.show})
+  }
   
   render() {
     return (
       <div>
-        <NavBar></NavBar>
-        <div class='card_list'>
-            <div class ='card text-black bg-info' style={{ width: '18rem' }}>
+        <NavBar/>
+        <div className='card_list'>
+            <div className='card text-black bg-info' style={{ width: '18rem' }}>
               <Card.Img src="animu_logo.png" alt="Logo" />
               <Card.Body>
-                <Card.Title><p class='title'>About me</p></Card.Title>
+                <Card.Title><p className='card_title'>About me</p></Card.Title>
                 <Card.Text>
                   <Container>
-                    <p class='info_grid'>
-                      <p class='attributeName redfont'>Name : </p>
-                      <p class='attributeValue'>Binaru Appuhamy</p>
+                    <p className='card_info_grid'>
+                      <p className='card_attribute_name red_font'>Name : </p>
+                      <p className='card_attribute_value'>Binaru Appuhamy</p>
                     </p>
-                    <p class='info_grid'>
-                      <p class='attributeName'>Current year : </p>
-                      <p class='attributeValue'>ECE PEY</p>
+                    <p className='card_info_grid'>
+                      <p className='card_attribute_name'>Current year : </p>
+                      <p className='card_attribute_value'>ECE PEY</p>
                     </p>
-                    <p class='info_grid'>  
-                      <p class='attributeName'>Hobbies : </p>
-                      <p class='attributeValue'>Read manga and watch anime!</p>
+                    <p className='card_info_grid'>  
+                      <p className='card_attribute_name'>Hobbies : </p>
+                      <p className='card_attribute_value'>Read manga and watch anime!</p>
                     </p>
                   </Container>
                 </Card.Text>
@@ -50,34 +49,37 @@ class HomePage extends React.Component {
                 </Accordion>
               </Card.Body>
             </div>
-            <div class ='card text-black bg-info' style={{ width: '18rem' }}>
-              <Card.Img class = "circle_crop" src="animu_logo.png" alt="Logo" />
+            <div className ='card text-black bg-info' style={{ width: '18rem' }}>
+              <Card.Img className="circle_crop" src="animu_logo.png" alt="Logo" />
               <Card.Body>
-                <Card.Title><p class='title'>About me</p></Card.Title>
+                <Card.Title><p className='card_title'>About me</p></Card.Title>
                 <Card.Text>
                   <Container>
-                    <p class='info_grid'>
-                      <p class='attributeName'>Name : </p>
-                      <p class='attributeValue'>Binaru Appuhamy</p>
+                    <p className='card_info_grid'>
+                      <p className='card_attribute_name'>Name : </p>
+                      <p className='card_attribute_value'>Binaru Appuhamy</p>
                     </p>
-                    <p class='info_grid'>
-                      <p class='attributeName'>Current year : </p>
-                      <p class='attributeValue'>ECE PEY</p>
+                    <p className='card_info_grid'>
+                      <p className='card_attribute_name'>Current year : </p>
+                      <p className='card_attribute_value'>ECE PEY</p>
                     </p>
-                    <p class='info_grid'>  
-                      <p class='attributeName'>Hobbies : </p>
-                      <p class='attributeValue'>Read manga and watch anime!</p>
+                    <p className='card_info_grid'>  
+                      <p className='card_attribute_name'>Hobbies : </p>
+                      <p className='card_attribute_value'>Read manga and watch anime!</p>
                     </p>
                   </Container>
                 </Card.Text>
-                <Accordion flush>
-                  <Accordion.Item eventKey="0">
-                    <Accordion.Header>More Hobbies</Accordion.Header>
-                    <Accordion.Body>
-                      Nope nothin here.
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Accordion>
+                <Button onClick={this.toggleshow} className="mb-2">
+                  Click me
+                </Button>
+                <Toast show={this.state.show} onClose={this.toggleshow}>
+                  <Toast.Header>
+                    <img className= "image_minify" src="animu_logo.png" alt="Logo"/>
+                    <strong className="me-auto">Binaru</strong>       
+                    <small>Just now</small>
+                  </Toast.Header>
+                  <Toast.Body>Wow you are reading this</Toast.Body>
+                </Toast>
               </Card.Body>
             </div>
         </div>

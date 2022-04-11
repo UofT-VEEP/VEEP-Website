2927bb9fe8d8b7f5262434ea7fdc7149baf347a6
Vicki Jin <jinsiqivicki@gmail.com>
2022-04-10 20:46:55 -0400

    add clickable linekdIn btn

--- src/Pages/TeamPage.js
+++ src/Pages/TeamPage.js
@@ -22,7 +22,11 @@ class TeamPage extends React.Component {
                             <Card.Title>{president.name}</Card.Title>
                             <Card.Subtitle>{president.year}</Card.Subtitle>
                             <Button href={"mailto:" + president.email}>Contact Me By Email</Button>
-                            <Button href={president.linkedIn}>Contact Me By linkedIn</Button>
+                            {/*<Button href={president.linkedIn}>Contact Me By linkedIn</Button>*/}
+                            <h5>Intro: I am xxxxx, xxxx </h5>
+                            <a href={president.linkedIn} target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+									<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+								  </svg></a>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -36,7 +40,10 @@ class TeamPage extends React.Component {
                         <Card.Body>
                             <Card.Title>{bdvp.name}</Card.Title>
                             <Card.Subtitle>{bdvp.year}</Card.Subtitle>
-                            <Button href={"mailto:" + bdvp.email}>Contact Me</Button>
+                            <Button href={"mailto:" + bdvp.email}>Contact Me By Email</Button>
+                            <a href={president.linkedIn} target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+									<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+								  </svg></a>
                         </Card.Body>
                         </Card>
                     </Col>
@@ -51,7 +58,10 @@ class TeamPage extends React.Component {
                             <Card.Body>
                                 <Card.Title>{eventvp.name}</Card.Title>
                                 <Card.Subtitle>{eventvp.year}</Card.Subtitle>
-                                <Button href={"mailto:" + eventvp.email}>Contact Me</Button>
+                                <Button href={"mailto:" + eventvp.email}>Contact Me By Email</Button>
+                                <a href={president.linkedIn} target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+									<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+								  </svg></a>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -66,7 +76,10 @@ class TeamPage extends React.Component {
                             <Card.Body>
                                 <Card.Title>{marketvp.name}</Card.Title>
                                 <Card.Subtitle>{marketvp.year}</Card.Subtitle>
-                                <Button href={"mailto:" + marketvp.email}>Contact Me</Button>
+                                <Button href={"mailto:" + marketvp.email}>Contact Me By Email</Button>
+                                <a href={president.linkedIn} target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+									<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+								  </svg></a>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -81,8 +94,10 @@ class TeamPage extends React.Component {
                             <Card.Body>
                                 <Card.Title>{techMan.name}</Card.Title>
                                 <Card.Subtitle>{techMan.year}</Card.Subtitle>
-                                <Button href={"mailto:" + techMan.email}>Contact Me</Button>
-                            </Card.Body>
+                                <Button href={"mailto:" + techMan.email}>Contact Me By Email</Button>
+                                <a href={president.linkedIn} target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+									<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+								  </svg></a>                            </Card.Body>
                         </Card>
                     </Col>
                 ))}

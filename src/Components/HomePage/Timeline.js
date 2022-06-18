d2feac71a547abe2b6de541aaec07ba4be544323
binaruappuhamy <binaruappuhamy>
2022-06-18 13:53:41 -0400

    Remove tooltip and add an arrow element on activeStep in the timeline to keep track of which timeline decsription is active

--- src/Components/HomePage/Timeline.js
+++ src/Components/HomePage/Timeline.js
@@ -3,8 +3,7 @@ import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepButton from '@mui/material/StepButton';
-import Tooltip from '@mui/material/Tooltip';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
+import ArrowDropUpRoundedIcon from '@mui/icons-material/ArrowDropUpRounded';
 import { Card } from 'react-bootstrap';
 
 import './Styles/Timeline.css';
@@ -22,59 +21,31 @@ class HorizontalNonLinearStepper extends React.Component {
     const {Steps, Important_Dates, Important_Desc} = this.props.data;
     const activeStep = this.props.activeStep;  
 
-    const handleUpdateToolTip = (index, value) => {
-      const updatedArray = [...this.state.popToolTip];
-      updatedArray[index] = value;
-      this.setState({popToolTip: updatedArray});
-    };
-
-    const handleTimelineClick = (index) => {
-      this.props.setActiveStep(index);
-      handleUpdateToolTip(index, true);
-    };
-
     return (
       <div>
-      <Box className="TimelineBox">
-        <Stepper nonLinear activeStep={activeStep} alternativeLabel>
-          {Steps.map((label, index) => (
-            <Step key={label}>
-              <Tooltip arrow title={Important_Desc[index]}>
-                <StepButton color="inherit" onClick= {() => this.props.setActiveStep(index)}>
-                  {label}<br/>({Important_Dates[index]})
-                </StepButton>
-              </Tooltip>
-              {/* <ClickAwayListener onClickAway={handleUpdateToolTip(index, false)}>
-            <div>
-              <Tooltip
-                PopperProps={{
-                  disablePortal: true,
-                }}
-                onClose={handleUpdateToolTip(index, false)}
-                open={this.state.popToolTip[index]}
-                disableFocusListener
-                disableHoverListener
-                disableTouchListener
-                title="test"
-                arrow
-              >
-                <StepButton color="inherit" onClick= {handleTimelineClick(index)}>
-                  {label}<br/>({Important_Dates[index]})
-                </StepButton>
-              </Tooltip>
-            </div>
-          </ClickAwayListener> */}
-            </Step>
-          ))}
-        </Stepper>
-      </Box>
-      <Card className="TimelineDesc">
-        <Card.Body>
-          <Card.Text>
-            {Important_Desc[activeStep]} 
-          </Card.Text>
-        </Card.Body>
-      </Card>
+        <Box className="TimelineBox">
+          <Stepper nonLinear activeStep={activeStep} alternativeLabel>
+            {
+              Steps.map(
+                (label, index) => (
+                  <Step key={label}>
+                    <StepButton color="inherit" onClick= {() => this.props.setActiveStep(index)}>
+                      {label}<br/>({Important_Dates[index]})
+                    </StepButton>
+                    {this.props.activeStep == index && <ArrowDropUpRoundedIcon style={{fontSize: 60}}/>}
+                  </Step>
+                )
+              )
+            }
+          </Stepper>
+        </Box>
+        <Card className="TimelineDesc">
+          <Card.Body>
+            <Card.Text>
+              {Important_Desc[activeStep]} 
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     )
   }

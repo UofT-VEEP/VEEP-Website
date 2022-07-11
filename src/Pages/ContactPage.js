0bae7a400a3819958758d263e0135d1b58c3066f
CatherineZM <49298813+CatherineZM@users.noreply.github.com>
2022-07-10 21:10:16 -0400

    Adjusted background image

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -32,31 +32,32 @@ class ContactPage extends React.Component {
 =======
         {/******WORD******/}
         <div  className="background">
-        <div className="contactWordLocation">
-          <Container className="contactWord">
-            <h1 class="display-3">Have any questions ?</h1>
+          <div className="contactWordLocation">
+            <Container className="contactWord">
+              <h1 class="display-3">Have any questions ?</h1>
 
-            <h2 class="display-5">we are happy to help <span class="display-1">:)</span></h2>
+              <h2 class="display-5">we are happy to help <span class="display-1">:)</span></h2>
 
-            <div className="contactInfo">
-              <img src="C001Email.svg" alt="Email-Logo" width="45" />
-              <span>Email: ############@######</span>
-            </div>
+              <div className="contactInfo">
+                <img src="C001Email.svg" alt="Email-Logo" width="45" />
+                <span>Email: ############@######</span>
+              </div>
 
-            <div className="contactInfo">
-              <img src="C002WeChat.svg" alt="Wechat-Logo" width="45" />
-              <span>Wechat ID: ############</span>
-            </div>
+              <div className="contactInfo">
+                <img src="C002WeChat.svg" alt="Wechat-Logo" width="45" />
+                <span>Wechat ID: ############</span>
+              </div>
 
-            <div>
-              <img className="imgmargin" src="C003Facebook.svg" alt="Facebook-Logo" width="50"></img>
-              <span className="Padding" />
-              <img className="imgmargin" src="C004Instgram.svg" alt="Instgram-Logo" width="50"></img>
-            </div>
+              <div>
+                <img className="imgmargin" src="C003Facebook.svg" alt="Facebook-Logo" width="50"></img>
+                <span className="Padding" />
+                <img className="imgmargin" src="C004Instgram.svg" alt="Instgram-Logo" width="50"></img>
+              </div>
 
-          </Container>
-        </div>
+            </Container>
+          </div>
 
+<<<<<<< HEAD
         {/******CARD******/}
         <div className="contactCardLocation">
           <Card className="contactCard">
@@ -69,6 +70,16 @@ class ContactPage extends React.Component {
 <<<<<<< HEAD
 >>>>>>> 004f5c2 (Contact Page New update)
 =======
+=======
+          {/******CARD******/}
+          <div className="contactCardLocation">
+            <Card className="contactCard">
+              <Card.Body>
+                <ContactForm />
+              </Card.Body>
+            </Card>
+          </div>
+>>>>>>> deb0910 (Adjusted background image)
         </div>
 >>>>>>> 0893659 (Contactpage update02)
       </div>
